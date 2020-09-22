e7804d15cd11515ea1515e502909e3de63c0e1c6
liangzi <liangzi@staff.sina.com.cn>
2020-09-22 14:17:05 +0800

    fix

--- lib/core/base_trainer/loss.py
+++ lib/core/base_trainer/loss.py
@@ -5,11 +5,12 @@ import torch.nn as nn
 class RMSELoss(nn.Module):
     def __init__(self, eps=1e-6):
         super().__init__()
-        self.mse = nn.MSELoss()
+        self.mse = nn.MSELoss(reduction='none')
         self.eps = eps
 
-    def forward(self, pre, target):
-        loss = torch.sqrt(self.mse(pre, target) + self.eps)
+    def forward(self, pre, target,weights):
+        weights=weights.unsqueeze(-1)
+        loss = torch.sqrt(torch.mean(self.mse(pre, target)*weights + self.eps))
         return loss
 
 class MCRMSELoss(nn.Module):
@@ -21,6 +22,5 @@ class MCRMSELoss(nn.Module):
     def forward(self, pre, target,weights):
         score = 0
         for i in range(self.num_scored):
-            score += self.rmse(pre[:, :, i], target[:, :, i]) / self.num_scored*weights
-
+            score += self.rmse(pre[:, :, i], target[:, :, i],weights) / self.num_scored
         return score
