dac552f7e1e81ac62087d8ac7c8c9e8e2624cecb
lz <2120140200@mail.nankai.edu.cn>
2020-10-11 22:34:33 +0800

    fix

--- lib/core/base_trainer/model.py
+++ lib/core/base_trainer/model.py
@@ -9,36 +9,101 @@ from efficientnet_pytorch import EfficientNet
 from train_config import config as cfg
 
 
+# A memory-efficient implementation of Swish function
+class SwishImplementation(torch.autograd.Function):
+    @staticmethod
+    def forward(ctx, i):
+        result = i * torch.sigmoid(i)
+        ctx.save_for_backward(i)
+        return result
+
+    @staticmethod
+    def backward(ctx, grad_output):
+        i = ctx.saved_variables[0]
+        sigmoid_i = torch.sigmoid(i)
+        return grad_output * (sigmoid_i * (1 + i * (1 - sigmoid_i)))
+
+class MemoryEfficientSwish(nn.Module):
+    def forward(self, x):
+        return SwishImplementation.apply(x)
+
+
+BN_MOMENTUM=0.03
+BN_EPS=1e-5
+ACT_FUNCTION=MemoryEfficientSwish
+
+
+class Attention(nn.Module):
+
+    def __init__(self, input_dim=512, output_dim=512):
+        super(Attention, self).__init__()
+
+        self.att=nn.Sequential(nn.Linear(input_dim, output_dim//4,bias=False),
+                               nn.BatchNorm1d(output_dim//4,momentum=BN_MOMENTUM,eps=BN_EPS),
+                               ACT_FUNCTION(),
+                               nn.Linear(output_dim//4, output_dim, bias=False),
+                               nn.BatchNorm1d(output_dim, momentum=BN_MOMENTUM,eps=BN_EPS),
+                               nn.Sigmoid())
+
+    def forward(self, x):
+        xx = self.att(x)
+
+        return x*xx
+
+
+
+class ResBlock(nn.Module):
+
+    def __init__(self, input_dim=512, output_dim=512):
+        super(ResBlock, self).__init__()
+
+        self.att=nn.Sequential(nn.Linear(input_dim, output_dim//4,bias=False),
+                               nn.BatchNorm1d(output_dim//4,momentum=BN_MOMENTUM,eps=BN_EPS),
+                               ACT_FUNCTION(),
+                               nn.Linear(output_dim//4, output_dim, bias=False),
+                               nn.BatchNorm1d(output_dim, momentum=BN_MOMENTUM,eps=BN_EPS),
+                               )
+        self.act=ACT_FUNCTION()
+    def forward(self, x):
+        xx = self.att(x)
+
+        return self.act(x+xx)
 
 class Complexer(nn.Module):
 
-    def __init__(self, num_features=874, num_targets=206, hidden_size=512):
+    def __init__(self, num_features=875, num_targets=206, hidden_size=512):
         super(Complexer, self).__init__()
         self.batch_norm1 = nn.BatchNorm1d(num_features)
-        self.dropout1 = nn.Dropout(0.2)
-        self.dense1 = nn.utils.weight_norm(nn.Linear(num_features, hidden_size))
+        self.dropout1 = nn.Dropout(0.3)
+        self.dense1 =nn.Sequential(nn.Linear(num_features, hidden_size,bias=False),
+                                   nn.BatchNorm1d(hidden_size,momentum=BN_MOMENTUM,eps=BN_EPS),
+                                   ACT_FUNCTION(),
+                                   nn.Dropout(0.5),
+                                   )
 
-        self.batch_norm2 = nn.BatchNorm1d(hidden_size)
-        self.dropout2 = nn.Dropout(0.5)
-        self.dense2 = nn.utils.weight_norm(nn.Linear(hidden_size, hidden_size))
+        self.dense2 =nn.Sequential(ResBlock(hidden_size,hidden_size),
+                                   nn.Dropout(0.5),
+                                   nn.Linear(hidden_size, hidden_size, bias=False),
+                                   nn.BatchNorm1d(hidden_size, momentum=BN_MOMENTUM, eps=BN_EPS),
+                                   ACT_FUNCTION(),
+                                   nn.Dropout(0.5),
+                                   ResBlock(hidden_size, hidden_size)
+                                   )
 
-        self.batch_norm3 = nn.BatchNorm1d(hidden_size)
-        self.dropout3 = nn.Dropout(0.5)
-        self.dense3 = nn.utils.weight_norm(nn.Linear(hidden_size, num_targets))
+        self.att=Attention(hidden_size,hidden_size)
+
+
+        self.dense3 = nn.Linear(hidden_size, num_targets)
 
     def forward(self, x):
 
 
-        x = self.batch_norm1(x)
+        #x = self.batch_norm1(x)
         x = self.dropout1(x)
-        x = F.relu(self.dense1(x))
+        x = self.dense1(x)
+        x =self.dense2(x)
 
-        x = self.batch_norm2(x)
-        x = self.dropout2(x)
-        x = F.relu(self.dense2(x))
-
-        x = self.batch_norm3(x)
-        x = self.dropout3(x)
+        x=self.att(x)
         x = self.dense3(x)
 
         return x
